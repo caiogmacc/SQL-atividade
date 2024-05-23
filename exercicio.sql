c4a33a0400ce0b326dcdc10127c71309b009a0bd
Caio Gonçalves Machado <144069563+caiogmacc@users.noreply.github.com>
2024-05-23 08:48:38 -0300

    commit

---
+++ exercicio.sql
@@ -0,0 +1,7 @@
+1-
+SELECT * FROM materias_alunos WHERE nota_prova_bimestral > 7
+
+2-
+SELECT * FROM materias_alunos WHERE ((nota_atitudinal+nota_licoes) + (nota_prova_mensal*3) + (nota_prova_bimestral*5) >= 70)
+
+3- 
